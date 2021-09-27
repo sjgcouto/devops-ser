a20a0cb3c2ce841926a344efb4bfb1cb9cb0d76b
sjgcouto <sgcouto@gmail.com>
2021-09-27 20:30:30 -0300

    Acerto 3

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/DevOps-ser/i);
+  const linkElement = screen.getByText(/Falha/i);
   expect(linkElement).toBeInTheDocument();
 });
